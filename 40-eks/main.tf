4001a4429bea7b64263d904d5f93d050c58d17f5
Harika Eravathri <harikaeravathri@gmail.com>
2024-11-25 13:01:19 -0800

    jenkins

--- 40-eks/main.tf
+++ 40-eks/main.tf
@@ -2,8 +2,9 @@ resource "aws_key_pair" "eks" {
   key_name   = "eks"
   # you can paste the public key directly like this
   #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6ONJth+DzeXbU3oGATxjVmoRjPepdl7sBuPzzQT2Nc sivak@BOOK-I6CR3LQ85Q"
-  public_key = file("~/.ssh/eks.pub")
+  // public_key = file("~/.ssh/eks.pub")
   # ~ means windows home directory
+   public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAUMgvJjTcexZRyCBqx6QJOh5633FpLnkA9/ubRWW+Xn harik@harika_anvesh"
 }
 
 module "eks" {
