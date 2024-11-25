5f02523413035548114d28235fa0337d97dfe940
Harika Eravathri <harikaeravathri@gmail.com>
2024-11-25 12:29:36 -0800

    jenkins

--- 70-ecr/main.tf
+++ 70-ecr/main.tf
@@ -1,7 +1,16 @@
 resource "aws_ecr_repository" "backend" {
-  name                 = "expense/backend"
+  name                 = "${var.project_name}/${var.environment}/backend"
   image_tag_mutability = "MUTABLE"
+  force_delete = true
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
 
+resource "aws_ecr_repository" "frontend" {
+  name                 = "${var.project_name}/${var.environment}/frontend"
+  image_tag_mutability = "MUTABLE"
+  force_delete = true
   image_scanning_configuration {
     scan_on_push = true
   }
